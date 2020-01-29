a0ba2599cdc550e6240909fd8294ef8cb18c3db7
MadRajib <madhabsharma94@gmail.com>
2020-01-30 00:03:52 +0530

    stack using circular queue done!

--- test.cpp
+++ test.cpp
@@ -1,36 +1,57 @@
+#include <iostream>
+
 class Queue{
     private:
-        int front;
-        int end;
+        int first;
+        int next;
         int *arr;
         int MAX_SIZE;
         int cnt;
     public:
         Queue() = default;
         Queue(int sz){
-            MAX_SIZE = sz;
+            MAX_SIZE = sz + 1;
             arr = new int[MAX_SIZE];
-            cnt = 0;
+            first = next = cnt = 0;
         }
 
         void enqueu(int v){
+            if(!isFull()){
+                arr[next] = v;
+                next = (next+1)%MAX_SIZE;
+                cnt ++;
+            }
 
         }
 
         int dequeue(){
-            return 0;
+            if(!isEmpty()){
+                int temp = arr[first];
+                first = (first+1)%MAX_SIZE;
+                cnt--;
+                return temp;
+            }
+
         }
 
         void print(){
-
+            for(int iter = first;iter!=next;){
+                std::cout<<arr[iter]<<" ";
+                iter = (iter+1)%MAX_SIZE;
+            }
+            std::cout<<std::endl;
         }
 
         bool isFull(){
-            return false;
+            return ((next+1)%MAX_SIZE) == first;
         }
 
         bool isEmpty(){
-            return false;
+            return first == next;
+        }
+
+        int getCount(){
+            return cnt;
         }
 
 };
@@ -38,37 +59,100 @@ class Queue{
 class Stack{
     public:
         Queue main_q,temp_q;
+        int MAX_SIZE;
+        int cnt;
         Stack(int sz){
-            main_q = Queue(sz);
-            temp_q = Queue(sz);
+            MAX_SIZE = sz;
+            main_q = Queue(MAX_SIZE);
+            temp_q = Queue(MAX_SIZE);
+            cnt = 0;
         }
 
         void push(int v){
-            if(!main_q.isFull()){
+            if(!isFull()){
                 main_q.enqueu(v);
+                cnt++;
+            }else{
+                std::cout<<"stack is full"<<std::endl;
             }
-            
         }
 
         int pop(){
-
+            if(!isEmpty()){
+                int c = cnt;
+                while(main_q.getCount()!=1){
+                    temp_q.enqueu(main_q.dequeue());
+                }
+                
+                Queue temp = temp_q;
+                temp_q = main_q;
+                main_q = temp;
+                cnt--;
+                return temp_q.dequeue();
+            }else{
+                std::cout<<"stack is empty!"<<std::endl;
+            }
         }
 
         bool isFull(){
-            return false;
+            return main_q.isFull();
         }
 
         bool isEmpty(){
-            return false;
+            return main_q.isEmpty();
         }
 
         void print(){
-
+            main_q.print();
         }
 
 };
 
 int main(){
-    int MAX_SIZE = 10;
+    int MAX_SIZE = 5;
     Stack stk = Stack(MAX_SIZE);
+    stk.push(1);
+    stk.print();
+    stk.push(2);
+    stk.print();
+
+    stk.push(3);
+    stk.print();
+
+    stk.push(4);
+    stk.print();
+
+    stk.push(5);
+    stk.print();
+
+    stk.pop();
+    stk.print();
+    stk.pop();
+    stk.print();
+    stk.pop();
+    stk.print();
+    stk.pop();
+    stk.print();
+    stk.pop();
+    stk.print();
+    stk.pop();
+    stk.print();
+    stk.pop();
+    stk.print();
+    stk.push(5);
+    stk.print();
+    stk.push(5);
+    stk.print();
+    stk.push(5);
+    stk.print();
+    stk.push(5);
+    stk.print();
+    stk.push(5);
+    stk.print();
+    stk.push(5);
+    stk.print();
+    stk.push(5);
+    stk.print();
+
+
 }
