fa5f554fdf6d0e27f644a98713ca8bd0dd42b13a
MadRajib <madhabsharma94@gmail.com>
2020-02-05 21:34:32 +0530

    sort cut macros added

---
+++ sortcuts.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#define REP(a,b) for(int i=a;i<b;i++)
+#define rREP(a,b) for(int i=a;i>=b;i--)
+#define FOR(i,n) REP(i,n)
+#define rFOR(i,n) rREP(i,n)
+#define vi vector<int>
+#define pb push_bach
+#define ll long long
+using namespace std;
+
+int main(){
+    rFOR(3,1){
+        cout<<1<<" ";
+    }
+        
+}
